af659469a7002870d479f331d234b1652268bc1b
ramity <lcb005@email.uark.edu>
2016-09-08 11:35:44 -0500

    error with elseif -> fixed to else if

--- gpa.cpp
+++ gpa.cpp
@@ -25,13 +25,13 @@ int main ()
 
   if(temp == "A")
     g1 = 5;
-  elseif(temp == "B")
+  else if(temp == "B")
     g1 = 4;
-  elseif(temp == "C")
+  else if(temp == "C")
     g1 = 3;
-  elseif(temp == "D")
+  else if(temp == "D")
     g1 = 2;
-  elseif(temp == "F")
+  else if(temp == "F")
     g1 = 1;
   else
   {
@@ -64,13 +64,13 @@ int main ()
 
   if(temp == "A")
     g2 = 5;
-  elseif(temp == "B")
+  else if(temp == "B")
     g2 = 4;
-  elseif(temp == "C")
+  else if(temp == "C")
     g2 = 3;
-  elseif(temp == "D")
+  else if(temp == "D")
     g2 = 2;
-  elseif(temp == "F")
+  else if(temp == "F")
     g2 = 1;
   else
   {
@@ -103,18 +103,18 @@ int main ()
 
   if(temp == "A")
     g3 = 5;
-  elseif(temp == "B")
+  else if(temp == "B")
     g3 = 4;
-  elseif(temp == "C")
+  else if(temp == "C")
     g3 = 3;
-  elseif(temp == "D")
+  else if(temp == "D")
     g3 = 2;
-  elseif(temp == "F")
+  else if(temp == "F")
     g3 = 1;
   else
   {
     cout << "You did not enter A or B or C or D or F.\n";
-    cout << "Your input was overridden as 'F'";
+    cout << "Your input was overridden as 'F'\n";
 
     g3 = 1;
   }
@@ -144,13 +144,13 @@ int main ()
 
   if(temp == "A")
     g4 = 5;
-  elseif(temp == "B")
+  else if(temp == "B")
     g4 = 4;
-  elseif(temp == "C")
+  else if(temp == "C")
     g4 = 3;
-  elseif(temp == "D")
+  else if(temp == "D")
     g4 = 2;
-  elseif(temp == "F")
+  else if(temp == "F")
     g4 = 1;
   else
   {
@@ -183,13 +183,13 @@ int main ()
 
   if(temp == "A")
     g5 = 5;
-  elseif(temp == "B")
+  else if(temp == "B")
     g5 = 4;
-  elseif(temp == "C")
+  else if(temp == "C")
     g5 = 3;
-  elseif(temp == "D")
+  else if(temp == "D")
     g5 = 2;
-  elseif(temp == "F")
+  else if(temp == "F")
     g5 = 1;
   else
   {
